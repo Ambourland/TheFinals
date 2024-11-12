ac8df410c3aa18b269566d5f53d2125905e33ae6
Ambourland <144298214+Ambourland@users.noreply.github.com>
2024-11-11 22:57:08 -0600

    add more to components cart, details and list to start

--- TheFinal/src/App.jsx
+++ TheFinal/src/App.jsx
@@ -1,13 +1,24 @@
 import Card from './components/Card'
 import './App.css'
+import Navbar from "./components/Navbar"
+import {BrowserRouter, Routes, Route} from 'react-router-dom' 
+import ProductList from './components/ProductList'
+import ProductDetails from './components/ProductDetails'
+import Cart from './components/Cart'
 
 function App() {
  
 
   return (
-    <>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route element={<ProductList />} path="/home"/>
+      <Route element={<ProductDetails />} path="/product-details"/>
+      <Route element={<Cart />} path="/cart"/>
+    </Routes>
      <Card />
-    </>
+    </BrowserRouter>
   )
 }
 
